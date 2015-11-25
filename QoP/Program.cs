50d1effdee566bdf7db7b904a99a3bb5a7fda15e
potassiumsquare <potassiumsquare@gmail.com>
2015-11-25 03:16:16 -0500

    1.0.0.2

--- QoP/Program.cs
+++ QoP/Program.cs
@@ -4,7 +4,6 @@ using Ensage;
 using SharpDX;
 using Ensage.Common.Extensions;
 using Ensage.Common;
-using Ensage.Common.Menu;
 using System.Windows.Input;
 using SharpDX.Direct3D9;
 
@@ -15,7 +14,7 @@ namespace QoP
     {
         private static bool _activated;
         private static Ability ss, blink, scream, sonic;
-        private static Item orchid, hex, bkb;
+        private static Item orchid, hex, bkb, mask, shivas, silveredge, shadowblade, mjo;
         private static Font _text;
         private const Key KeyCombo = Key.Space;
         private const Key BkbToggleKey = Key.F;
@@ -54,8 +53,7 @@ namespace QoP
             if (!_activated) return;
             if ((_target == null || !_target.IsVisible))
                 _me.Move(Game.MousePosition);
-            if (_target == null || _me.Distance2D(_target) > 1300)
-                _target = _me.ClosestToMouseTarget(1000);
+            _target = _me.ClosestToMouseTarget(1000);
             //Skills & Items
             ss = _me.Spellbook.Spell1; //Shadow Strike
             blink = _me.Spellbook.Spell2; //Blink
@@ -64,6 +62,12 @@ namespace QoP
             orchid = _me.FindItem("item_orchid"); //Orchid [Silence]
             hex = _me.FindItem("item_sheepstick"); //Hex [Disable]
             bkb = _me.FindItem("item_black_king_bar"); //BKB [Magic Immune]
+            mask = _me.FindItem("item_mask_of_madness"); //Mask of Madness [Attack Speed]
+            mjo = _me.FindItem("item_mjollnir"); //Mjollnir Buff [AoE lightning]
+            shivas = _me.FindItem("item_shivas_guard"); //Shivas [AoE Slow]
+            shadowblade = _me.FindItem("item_silver_edge"); //Shadow BlaDE [Shadow Walk]
+            silveredge = _me.FindItem("item_item_silver_edge"); //Silver Edge [Shadow Walk + Break]
+            var isInvis = _me.IsInvisible(); //Check For ShadowBlade/Silver Edge Use
             if (_target != null && _target.IsAlive && !_target.IsIllusion && !_target.IsMagicImmune())
             {
                 if (!Utils.SleepCheck("QoP")) return;
@@ -77,6 +81,22 @@ namespace QoP
                     blink.UseAbility(_target.Position);
                     Utils.Sleep(150 + Game.Ping, "blink");
                 }
+                //Use Shadowblade
+                else if (shadowblade != null && shadowblade.CanBeCasted() && Utils.SleepCheck("shadowblade"))
+                {
+                    shadowblade.UseAbility();
+                    Utils.Sleep(150 + Game.Ping, "sonic");
+                    if (isInvis)
+                        _me.Attack(_target);
+                }
+                //Use Silver Edge
+                else if (silveredge != null && silveredge.CanBeCasted() && Utils.SleepCheck("silveredge"))
+                {
+                    silveredge.UseAbility();
+                    Utils.Sleep(150 + Game.Ping, "silveredge");
+                    if (isInvis)
+                        _me.Attack(_target);
+                }
                 //Check to Use BKB
                 else if (bkb !=null && _bkbToggle && Utils.SleepCheck("BKB"))
                 {
@@ -88,6 +108,12 @@ namespace QoP
                 {
                     orchid.UseAbility(_target);
                     Utils.Sleep(150 + Game.Ping, "orchid");
+                }
+                //Use Shivas
+                else if (targetDistance <= attackRange && shivas != null && shivas.CanBeCasted() && Utils.SleepCheck("shivas"))
+                {
+                    shivas.UseAbility();
+                    Utils.Sleep(150 + Game.Ping, "shivas");
                 }
                 //Use Hex
                 else if (targetDistance <= attackRange && hex != null && hex.CanBeCasted() && Utils.SleepCheck("hex"))
@@ -113,12 +139,25 @@ namespace QoP
                     sonic.UseAbility(_target.Position);
                     Utils.Sleep(150 + Game.Ping, "sonic");
                 }
+                //Use Mjollnir
+                else if (mjo != null && mjo.CanBeCasted() && Utils.SleepCheck("mjo"))
+                {
+                    mjo.UseAbility(_me);
+                    Utils.Sleep(150 + Game.Ping, "mjo");
+                }
+                //Use Mask of Madness
+                else if (mask != null && mask.CanBeCasted() && Utils.SleepCheck("mask"))
+                {
+                    mask.UseAbility();
+                    Utils.Sleep(150 + Game.Ping, "mask");
+                }
                 else if (nothingCanCast())
                 {
                     _me.Attack(_target);
                 }
             }
         }
+        //NothingCanCast Function
         private static bool nothingCanCast()
         {
             if (!ss.CanBeCasted() &&
@@ -126,7 +165,9 @@ namespace QoP
                 !scream.CanBeCasted() &&
                 !sonic.CanBeCasted() &&
                 !orchid.CanBeCasted() &&
-                !hex.CanBeCasted())
+                !hex.CanBeCasted() &&
+                !shivas.CanBeCasted() &&
+                !mask.CanBeCasted())
                 return true;
             else
             {
